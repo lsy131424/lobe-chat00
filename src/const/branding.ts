159953f7c01a56bacc3df4f16e17ac457a92e3f1
lsy131424 <3309778572@qq.com>
2025-04-03 11:53:32 +0800

    Update branding.ts

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = '绘趣故事馆——大模型驱动的图画故事生成创作平台 Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = '绘趣故事馆——大模型驱动的图画故事生成创作平台';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = '绘趣故事馆——大模型驱动的图画故事生成创作平台';
 
 export const BRANDING_URL = {
   help: undefined,
